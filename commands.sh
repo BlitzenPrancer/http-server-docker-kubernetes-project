e67bc0613b43a481b46f80dcf989344fa3cc0a5e
Blitzen Prancer <76688640+BlitzenPrancer@users.noreply.github.com>
2021-08-15 20:42:28 +0530

    Delete commands.sh

--- commands.sh
+++
@@ -1,9 +0,0 @@
-docker build --no-cache -t blitzenprancer/http-server-docker-kubernetes-project:v1 .
-
-docker push blitzenprancer/http-server-docker-kubernetes-project:v1
-
-kubectl delete deployment http-server-docker-kubernetes-project
-
-kubectl apply -f devops/deployment.yaml
-
-kubectl apply -f devops/service.yaml
